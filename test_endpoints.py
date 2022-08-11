1ee8c9f632274ec067b03bb1cbf530f3fec9f9f5
Monouco <algoalgo175@gmail.com>
2022-08-11 16:48:41 -0500

    Agregando pruebas automatizadas

---
+++ test_endpoints.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+import json
+from main import app
+
+client = TestClient(app)
+
+def test_post_prob_enfermedad_cardiaca():
+    with open('test_input.json') as file:
+        data = json.load(file)
+    
+    response = client.post("/probEnfermedadCardiaca", json=data)
+    assert response.status_code == 200
