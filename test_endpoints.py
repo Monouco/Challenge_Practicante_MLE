9879117c9e62a437ba6f05ec6ebdbad4dd551b78
Monouco <algoalgo175@gmail.com>
2022-08-11 23:25:36 -0500

    Actualizando pruebas del servicio

--- test_endpoints.py
+++ test_endpoints.py
@@ -5,8 +5,10 @@ from main import app
 client = TestClient(app)
 
 def test_post_prob_enfermedad_cardiaca():
-    with open('test_input.json') as file:
+    with open('testsData/test_input.json') as file:
         data = json.load(file)
     
     response = client.post("/probEnfermedadCardiaca", json=data)
-    assert response.status_code == 200
+    assert response.status_code == 200, "Error con el servicio"
+    assert type(response.json()['prob']) is float, "La probabilidad debe ser un numero decimal"
+    assert response.json()['prob'] <= 1, "La probabilidad supera a 1"
